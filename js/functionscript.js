7c3b4e5d3041fce3781061ec4222cc812a8a4135
Viktor Zherdev <vrzherdev@gmail.com>
2014-01-31 17:21:23 +0200

    удалил блок с днями недели
    Signed-off-by: Viktor Zherdev <vrzherdev@gmail.com>

--- js/functionscript.js
+++ js/functionscript.js
@@ -26,9 +26,7 @@
                   .text(modalText);
           };
 
-          //array for connection with a date
-          //var dts = dt.toString(); 			
-          var weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
 
           //generating the array of hours
           var dts = ["09:00 - 10:00"];
@@ -38,35 +36,46 @@
 
 
           // creating timetable #1 
-          var table = $('<table class="table-hover"><th colspan=4><form><p>Select a date in the datepicker or </br>a weekday on the right   <span class="glyphicon glyphicon-arrow-right"></span></p></form></th></table>');
+          var table = $('<table class="table-hover"><th colspan=4><p>Select a date in the datepicker or </br>a weekday on the right&nbsp;&nbsp; <span class="glyphicon glyphicon-arrow-right"></span></p></th></table>');
           var i = -1;
+
           $(dts)
               .each(function () {
                   i++;
                   var row = $("<tr><td>available</td></tr>");
-                  $('<td><input type="button" value ="select" class="btn"></td><td><input type="button" value ="reset" class="btn"></td>')
-                      .appendTo(row);
                   $('<td></td>')
                       .append(dts[i])
                       .prependTo(row);
+                  $('<td><button id="sel" class ="btn">select</button></td>')
+                      .appendTo(row);
+                  $('<td><button id="res" class ="btn">reset</button</td>')
+                      .appendTo(row);
                   row.appendTo(table);
               });
-
           $(table)
               .appendTo('#timetable');
 
+          // buttons book and cancel of timetable #1	
+
+          $("#btnrow")
+              .appendTo('#timetable');
+
           // adding login and password form
-          $('<div id ="log"><form role="form" id ="logform" ><input type="text" name="login" class="loginform" placeholder="enter email here" autofocus required><input type="text" name="password" class="loginform" placeholder="enter password here" required><br><span><button type="submit" class="btn log">login</button><button type="reset" class="btn log">reset</button></form><input id="reginlog" class="btn log" type ="button" value = "register"></span></div>')
+          $("#log")
               .prependTo(table);
+
 
           // button register in login form
           $("#reginlog")
               .click(function () {
+                  //hiding login form
                   $('#logform')
-                      .toggle();
-                  // adding registration form to table #1
-                  $('<form id ="reginform"><input type="text" name="firstname" placeholder="first name here" class="regform"  autofocus required><input type="text" name="lastname" placeholder="last name here" class="regform" required></br><input type="email" name="email" placeholder="e-mail address here" class="regform" required><input type="text" name="phone" placeholder="phone number here" class="regform" required></br><input type="password" name="password" placeholder="password here" class="regform" required><input type="password" name="confpassword" placeholder="confirm password here" class="regform" required></br><button id="regbtn2" class="btn log">register</button><input type="reset" class="btn log" value="reset"><button id="closereg" class="btn log">close</button></form>')
-                      .appendTo('#log');
+                      .hide();
+
+                  // displaying registration form 
+                  $("#reginform")
+                      .show();
+
 
                   // button register in registration form
                   $("#regbtn2")
@@ -149,100 +158,30 @@
                   // button close in registration form
                   $("#closereg")
                       .click(function () {
+                          //hiding registration form
                           $("#reginform")
-                              .detach();
+                              .hide();
+                          // displaying login form 
                           $('#logform')
-                              .toggle();
+                              .show();
                       });
 
               });
 
 
-          //Table #2 for booking according to weekdays
-          var wtable = $('<table><th><p>Select a day for periodic booking</p></th></table>');
-          var wselect = $('<select id="select" size="6" name="week[]" multiple></select>');
-          var k = -1;
-          $(weekday)
-              .each(function () {
-                  k++;
-                  $('<option> </option>')
-                      .append(weekday[k])
-                      .appendTo(wselect);
-              });
-          (wselect)
-              .wrap('<td></td>')
-              .wrap('<tr></tr>')
-              .appendTo(wtable);
-          var wtr = $('<span id="wbtn"></span>');
-          // buttons of table #2 selection according to a weekday
-          $('<input type="button" id="sel" class="btn" value ="select">')
-              .wrap('<td></td>')
-              .prependTo(wtr);
-          $('<input type="button" id="res" class="btn" value="reset">')
-              .wrap('<td></td>')
-              .appendTo(wtr);
-          (wtr)
-              .appendTo(wtable);
-          (wtable)
-              .appendTo("#wtable");
-
-
-          // button events of table #2 selection according to a weekday
-          // select button 
-          $("#sel")
-              .click(function () {
-                  // this resets selections
-                  selCancel();
-
-                  //destroying tooltip on select button
-                  $('#timetable table tr input:even')
-                      .tooltip('destroy');
-
-
-                  var wday = $("#select option:selected")
-                      .text();
-                  if (!(wday)
-                      .length) {
-                      modalText = "Please, make WEEKDAY(s) selected";
-                      modalSet(modalText);
-                  } else {
-                      dt = $("#date")
-                          .text(wday);
-                  }
-
-              });
-
-
-
-          // reset button 
-          $("#res")
-              .click(function () {
-                  var sel = document.getElementById('date');
-                  sel.innerHTML = null;
-                  return true;
-              });
-
-
-
 
           //tooltip for button select
-          $('#timetable table tr input:even')
+          $('#sel.btn')
               .tooltip({
                   'show': true,
                   'placement': 'top',
                   'title': 'Select a date or weekday before selecting hours, then press "book"'
               });
 
-          //tooltip for button select of table#2
-          $("#sel")
-              .tooltip({
-                  'show': true,
-                  'placement': 'top',
-                  'title': "Select a weekday before pressing the button"
-              });
+
 
           // click events of timetable #1: button select
-          $("#timetable table tr input:even")
+          $("button#sel")
               .on("click", function () {
 
                   if (!$(dt)
@@ -260,7 +199,7 @@
                       .parent('td')
                       .prevAll()
                       .css("color", '#428bca');
-                  //destroying tooltip on select button
+                  //destroying tooltip on book button
                   $('#book')
                       .tooltip('destroy');
 
@@ -269,7 +208,7 @@
 
 
           // click events of timetable #1: button reset
-          $("#timetable table tr input:odd")
+          $("button#res")
               .on("click", function () {
                   $(this)
                       .closest('tr')
@@ -278,12 +217,9 @@
                   $(this)
                       .parent('td')
                       .prevAll()
-                      .css("color", 'black');
+                      .css("color", 'white');
               });
 
-          // buttons book and cancel of timetable #1
-          $('<tr><td colspan=2><input id ="book" type="submit" value="book" class="btn"></td><td colspan=2><input id ="cnl" type="reset" value="cancel" class="btn"></td></tr>')
-              .appendTo(table);
 
 
 
@@ -360,13 +296,11 @@
           $(modalDialog)
               .appendTo("#myModal");
 
-          <!-- Not to conflict with other scripts -->	
-
 
 
 
           // jQuery UI datepicker
-          $('#calendar')
+          $('#ui-datepicker-div')
               .datepicker({
                   inline: true,
                   showWeek: true,
@@ -382,7 +316,7 @@
                       selCancel();
 
                       //destroying tooltip on select button
-                      $('#timetable table tr input:even')
+                      $('#book')
                           .tooltip('destroy');
 
                       // aligning date string
@@ -405,6 +339,7 @@
  
  moment(dt);*/
                   }
+
               });
 
 
